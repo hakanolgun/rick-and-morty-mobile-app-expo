412443a7720ef903d235cf9c4e49ab0e9d2eaaf7
hakanolgun <hakanolgun@tutamail.com>
2023-03-25 11:35:41 +0300

    Change Character Card design

--- components/character/CharacterCard.tsx
+++ components/character/CharacterCard.tsx
@@ -2,7 +2,8 @@ import {StyleSheet, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {ICharacter} from '../../interface/episode';
 import {Link} from 'expo-router';
-
+import {Avatar, Button, Card, Text} from 'react-native-paper';
+import Colors from '../../constants/Colors';
 interface IProps {
   char: ICharacter;
   children: any;
@@ -13,26 +14,31 @@ const CharacterCard = ({char, children}: IProps) => {
 
   return (
     <Link href={{pathname: '/characters', params: {charID: char.id}}} asChild>
-      <TouchableOpacity style={ss.btn}>
-        <Image source={{uri: charImgUrl}} style={ss.img} />
+      <Card style={ss.card}>
+        <Card.Content style={ss.header}>
+          <Text variant="titleLarge">{char.name}</Text>
+          <Text variant="bodyMedium">{char.species ?? ''}</Text>
+        </Card.Content>
+        <Card.Cover source={{uri: charImgUrl}} style={ss.img} />
         {children}
-      </TouchableOpacity>
+      </Card>
     </Link>
   );
 };
 
 export default CharacterCard;
 const ss = StyleSheet.create({
+  card: {
+    marginBottom: 16,
+    marginHorizontal: 4,
+    backgroundColor: Colors.main,
+  },
+  header: {
+    justifyContent: 'center',
+    paddingBottom: 10,
+  },
   img: {
     width: '100%',
-    height: '100%',
-    borderRadius: 6,
-  },
-  btn: {
-    marginTop: 10,
-    width: '100%',
     height: 300,
-    borderRadius: 6,
-    position: 'relative',
   },
 });
