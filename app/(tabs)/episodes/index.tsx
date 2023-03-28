0c824d92aeb4cafdf57f69287eaf7e72a8712537
hakanolgun <hakanolgun@tutamail.com>
2023-03-28 17:47:52 +0300

    Configure app to receive push notifications
    In the home screen, I import necessary libraries and create
    functions to listen incoming push notifications.
    
    I create another file for sending push notifications. It accepts
    three parameter at the moment but It can be expandible for future
    functionalities like interactive push messages etc.

--- app/(tabs)/episodes/index.tsx
+++ app/(tabs)/episodes/index.tsx
@@ -1,11 +1,57 @@
-import {View, FlatList, SafeAreaView, RefreshControl} from 'react-native';
+import {View, FlatList, SafeAreaView, RefreshControl, Platform} from 'react-native';
 import React, {useEffect, useState} from 'react';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import base from '../../../constants/Base';
 import ShowMsg from '../../../components/common/ShowMsg';
 import {IEpisode} from '../../../interface/episode';
 import EpisodeCard from '../../../components/episode/EpisodeCard';
 import Search from '../../../components/common/Search';
 import useEpisodes from '../../../hooks/useEpisodes';
+import {useAppDispatch} from '../../../store/hookTypes';
+import {changeToken, setNotification} from '../../../store/slices/notifySlice';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const {status: existingStatus} = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const {status} = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: 'af03344a-5424-4d70-8585-fe4bf9929f6f',
+      })
+    ).data;
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 const EpisodesScreen = () => {
   const [page, setPage] = useState(1);
@@ -13,6 +59,18 @@ const EpisodesScreen = () => {
   const [searchValue, setSearchValue] = useState('');
   const [refreshing, setRefreshing] = useState(false);
   const [searchCount, setSearchCount] = useState(1);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => dispatch(changeToken(token)));
+
+    const subscription = Notifications.addNotificationReceivedListener(notification => {
+      dispatch(setNotification(notification));
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   const loadMore = () => {
     if (!loading && page < 3 && info?.next) {
       setPage(p => p + 1);
---
+++ utils/notifications.ts
@@ -0,0 +1,19 @@
+export async function sendPushNotification(expoPushToken: string, title: string, body: string) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: title,
+    body: body,
+    data: {data: 'Rick And Morty'},
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
